ecd16c8575afd71fe6173ca7c9920cf6414d8508
KyawTun11 <88315361+KyawTun11@users.noreply.github.com>
2021-08-09 08:43:03 +0630

    UI1

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:new_ui/screen/singup.dart';
 
 import 'login_screen.dart';
 
@@ -14,93 +15,106 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.all(32),
-          child: Column(
-            children: <Widget>[
-              ClipPath(
-                child: Image(
-                  height: MediaQuery.of(context).size.height / 2,
-                  width: double.infinity,
-                  image: AssetImage('assets/images/head.ico'),
-                ),
-              ),
-              Expanded(
-                child: Align(
-                  alignment: FractionalOffset.topCenter,
-                  child: GestureDetector(
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: 30.0,
-                      child: Text(
-                        'Discover Upcoming Events Near You',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: FlatButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()));
-                  },
-                  child: Text(
-                    'Sing Up',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                  color: Colors.blue,
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                  color: Colors.blue,
-                ),
-              ),
-              Expanded(
-                child: Align(
-                  alignment: FractionalOffset.bottomCenter,
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      alignment: Alignment.bottomCenter,
-                      height: 20.0,
-                      child: Text(
-                        'Skip For Now',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
+      body: Stack(
+        children: [
+          buildLogoArea(),
+          buildButtonsArea(),
+        ],
       ),
     );
   }
+
+  Widget buildLogoArea() => Align(
+        alignment: Alignment.center,
+        child: Container(
+          padding: const EdgeInsets.all(46),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: buildLogoImage(),
+              ),
+              buildText('Discover upcoming events near you'),
+            ],
+          ),
+        ),
+      );
+
+  Widget buildLogoImage() => Image(
+        height: 128,
+        image: AssetImage('assets/images/flutter.png'),
+      );
+
+  Widget buildText(String text) => Container(
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 22.0,
+            fontWeight: FontWeight.normal,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      );
+  Widget buildSignup() => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+          minimumSize: Size(double.infinity, 46),
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+        ),
+        child: Text('Sign Up'),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginScreen()));
+        },
+      );
+  Widget buildButton() => OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+          minimumSize: Size(double.infinity, 46),
+          backgroundColor: Colors.white,
+          primary: Colors.blue,
+          side: BorderSide(color: Colors.blue),
+        ),
+        child: Text('Login'),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SingUp()));
+        },
+        onLongPress: () {},
+      );
+  Widget buildButtonsArea() => Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          margin: EdgeInsets.all(46),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: buildSignup(),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: buildButton(),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  primary: Colors.blue,
+                ),
+                child: Text('Skip now'),
+                onPressed: () {},
+                onLongPress: () {},
+              ),
+            ],
+          ),
+        ),
+      );
 }
--- lib/screen/singup.dart
+++ lib/screen/singup.dart
@@ -1,142 +1,234 @@
 import 'package:flutter/material.dart';
-
-class SingUp extends StatefulWidget {
-  const SingUp({Key? key}) : super(key: key);
+import 'package:new_ui/screen/login_screen.dart';
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  _SingUpState createState() => _SingUpState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _SingUpState extends State<SingUp> {
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(
-            children: <Widget>[
-              ClipPath(
-                child: Image(
-                  height: MediaQuery.of(context).size.height / 2,
-                  width: double.infinity,
-                  image: AssetImage('assets/images/head.ico'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    SizedBox(
-                      width: double.infinity,
-                      child: FlatButton(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            contentPadding:
-                                EdgeInsets.symmetric(horizontal: 10),
-                            fillColor: Colors.white,
-                            hintText: 'Email',
-                          ),
-                          obscureText: true,
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: FlatButton(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            contentPadding:
-                                EdgeInsets.symmetric(horizontal: 10),
-                            fillColor: Colors.white,
-                            hintText: 'Password',
-                          ),
-                          obscureText: true,
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Text(
-                '....or....',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  color: Colors.blue,
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Login With Facebook',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  color: Colors.blue,
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Login With Google',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                  color: Colors.blue,
-                ),
-              ),
-              RichText(
-                text: TextSpan(
-                  text: 'Don`t have an acount? ',
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'Sing Up',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+      body: Stack(
+        children: [
+          buildScrollView(),
+          buildLoginText(),
+        ],
       ),
     );
   }
+
+  Widget buildScrollView() => SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.all(10),
+          child: Container(
+              child: Column(
+            children: <Widget>[
+              buildImage(),
+              buildTextField('Full Name'),
+              buildText('E_mail'),
+              buildPasswordButton('Password'),
+              buildSingUp(),
+              buildCanterText(),
+              buildFaceButton(),
+              buildGoogleButton(),
+              buildRichText(),
+              buildPolicyText('Privacy Policy'),
+            ],
+          )),
+        ),
+      );
+
+  Widget buildImage() => Padding(
+        padding: const EdgeInsets.fromLTRB(0, 64, 0, 32),
+        child: ClipPath(
+          child: Image(
+            height: 128,
+            image: AssetImage('assets/images/flutter.png'),
+          ),
+        ),
+      );
+  Widget buildTextField(String name) => Padding(
+        padding: const EdgeInsets.all(8),
+        child: Container(
+          //height: 64,
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: name,
+              hintStyle: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+              fillColor: Color(0xFFDCE3EC),
+              filled: true,
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+      );
+  Widget buildText(String hintText) => Padding(
+        padding: const EdgeInsets.all(8),
+        child: Container(
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+              fillColor: Color(0xFFDCE3EC),
+              filled: true,
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+      );
+  Widget buildPasswordButton(String password) => Padding(
+        padding: const EdgeInsets.all(4),
+        child: Container(
+          child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: password,
+              hintStyle: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+              suffixIcon: Icon(
+                Icons.visibility,
+                size: 35,
+              ),
+              fillColor: Color(0xFFDCE3EC),
+              filled: true,
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+      );
+  Widget buildSingUp() => Padding(
+        padding: const EdgeInsets.all(4),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
+            ),
+            minimumSize: Size(double.infinity, 46),
+            primary: Colors.blue,
+            onPrimary: Colors.white,
+          ),
+          child: Text('Sign Up'),
+          onPressed: () {},
+        ),
+      );
+  Widget buildCanterText() => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          '________________Or________________',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 20,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      );
+  Widget buildFaceButton() => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            primary: Color(0xFF4a6ea8),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
+            ),
+            minimumSize: Size(double.infinity, 45),
+          ),
+          icon: Icon(Icons.facebook, size: 32),
+          label: Text('Sing Up with Facebook'),
+          onPressed: () {},
+          onLongPress: () {},
+        ),
+      );
+  Widget buildGoogleButton() => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: OutlinedButton.icon(
+          style: OutlinedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
+            ),
+            minimumSize: Size(double.infinity, 46),
+            backgroundColor: Colors.white,
+            primary: Colors.blue,
+            side: BorderSide(color: Colors.blue),
+          ),
+          icon: Image.asset(
+            'assets/images/Flat.png',
+            height: 24,
+            alignment: Alignment.topLeft,
+          ),
+          label: Text('Sing up with Google'),
+          onPressed: () {},
+          onLongPress: () {},
+        ),
+      );
+  Widget buildRichText() => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: RichText(
+          text: TextSpan(
+            text: 'By Singing up you accept the',
+            style: TextStyle(fontSize: 15, color: Colors.black),
+            children: <TextSpan>[
+              TextSpan(
+                text: ' Terms of Service',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              TextSpan(text: '  and'),
+            ],
+          ),
+        ),
+      );
+  Widget buildPolicyText(String policy) => Text(
+        policy,
+        style: TextStyle(
+            color: Colors.blue,
+            fontSize: 16,
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.bold),
+      );
+  Widget buildLoginText() => GestureDetector(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LoginScreen(),
+          ),
+        ),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: RichText(
+              text: TextSpan(
+                text: 'Already have an account? ',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'Login',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
 }
